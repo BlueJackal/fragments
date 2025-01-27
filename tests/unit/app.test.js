6591bc9dee262ecb3108d254b1f8c1067502ba3e
Chris Simon <Chris.m.simon1@gmail.com>
2025-01-27 17:52:46 -0500

    Fixed app.test.js

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -4,7 +4,7 @@ const app = require('../../src/app');
 describe('404 handler', () => {
   test('should return 404 for non-existent routes', async () => {
     const res = await request(app).get('/non-existent-route');
-    expect(res.statusCode).toBe(500);
+    expect(res.statusCode).toBe(404);
     expect(res.body).toEqual({
       status: 'error',
       error: {
